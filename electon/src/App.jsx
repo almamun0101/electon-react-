788791a103a6f91db99d02385f110c2c4a271049
Al Mamun Khan <almamunkhan0101@gmail.com>
2025-03-03 17:37:27 +0600

    ai

--- electon/src/App.jsx
+++ electon/src/App.jsx
@@ -1,10 +1,13 @@
 import Header from './components/Header'
+import Sldieshow from './components/Slideshow'
 import './App.css'
 
 function App() {
   return (
     <>
       <Header/>
+      <Sldieshow/>
+      
     </>
   )
 }
---
+++ electon/src/components/Slideshow.jsx
@@ -0,0 +1,43 @@
+import img1 from './Images/item1.png'
+
+function Sldieshow(){
+    return(
+        <>
+        <section id="slideshow">
+            <div className="container">
+                <div className="relative">
+                    <button
+                        className="absolute top-15 border-1 border-transparent rounded-full w-10 h-10 bg-[#EAEAEA] -left-5">
+                        <i className="fa-solid fa-arrow-left"></i>
+                    </button>
+                    <button
+                        className="absolute top-15 border-1 border-transparent rounded-full w-10 h-10 bg-[#EAEAEA] -right-5">
+                        <i className="fa-solid fa-arrow-right"></i>
+                    </button>
+                    <div className="grid grid-cols-3 gap-5">
+                    <Card product={{name: "Laptop", quantity: "6 Item"}}/>
+                    
+             </div>
+                </div>
+            </div>
+
+        </section>
+
+
+        </>
+    )
+}
+
+function Card({product}){
+    return(
+                <div className="border-1 border-pri-500 rounded-4xl py-5 px-5 flex items-center justify-around">
+                    <img src={img1} alt="speaker" />
+                    <div className="text-pri-500 text-2xl">
+                        <h2>{product.name}</h2>
+                        <h2>{product.quantity}</h2>
+                    </div>
+                </div>
+    )
+}
+
+export default Sldieshow
