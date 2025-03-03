015570c64a0aa0537734ec71629ebf1386ab31b3
Al Mamun Khan <almamunkhan0101@gmail.com>
2025-03-04 01:27:50 +0600

    ain

--- electon/src/App.jsx
+++ electon/src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header'
 import Sldieshow from './components/Slideshow'
 import Product from './components/Product'
 import Banner from './components/Banner'
+import Review from './components/Review'
 import './App.css'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Sldieshow/>
       <Product/>
       <Banner />
+      <Review />
     </>
   )
 }
---
+++ electon/src/components/Review.jsx
@@ -0,0 +1,27 @@
+function Review(){
+    return(
+        <>
+        <div className="container">
+            <div className="bg-[#E2F4FF] p-5">
+                <div className="grid grid-cols-3 gap-5">
+                    
+
+                </div>
+
+            </div>
+
+        </div>
+
+
+        </>
+    )
+}
+
+function review(){
+    return(
+        <div className="flex items-center gap-5">
+            
+        </div>
+    )
+}
+export default Review
