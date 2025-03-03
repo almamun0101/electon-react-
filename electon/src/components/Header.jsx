2fcbdc5d331c9a2f994c7513b7edd56bd9998100
Al Mamun Khan <almamunkhan0101@gmail.com>
2025-03-03 13:48:09 +0600

    ain

---
+++ electon/src/components/Header.jsx
@@ -0,0 +1,33 @@
+function Header(){
+    return(
+        <>
+            <div className="container">
+                <div className="flex justify-between py-5">
+                <div className="text">
+                    <h3>
+                    Need help? Call us: (+98) 0234 456 789
+                    </h3>
+                </div>
+                <div className="flex items-center gap-20">
+                    <div className="flex items-center gap-5">
+                    <i className="fa-solid fa-location-dot"></i>
+                    <h3>Our Store</h3>
+                    </div>
+                    <div className="flex items-center gap-5">
+                    <i className="fa-solid fa-truck-fast"></i>
+                    <h3>Track your Order</h3>
+                    </div>
+                </div>
+            </div>
+  </div>
+        
+        
+        
+        
+        
+        
+        
+        </>
+    )
+}
+export default Header
