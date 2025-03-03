a46bf1d7bcb27c15d721407ae2aea619c1534c54
Al Mamun Khan <almamunkhan0101@gmail.com>
2025-03-04 00:28:46 +0600

    ain

--- electon/src/components/Banner.jsx
+++ electon/src/components/Banner.jsx
@@ -10,9 +10,43 @@ function Banner(){
                     <h2 className="text-6xl font-bold py-6 text-[#2E8FC5]">Sale up to 50% off</h2>
                     <h4 className="py-6">12 inch hd display</h4>
                     <Btn name="Shop Now"/>
-
                 </div>
             </div>
+
+            <div class="flex w-full py-20 gap-5">
+                <div class="w-2/3 border border-[#B6B6B6] rounded-2xl p-10 flex">
+                    <img src="./Images/speaker.png" alt="" />
+                    <div className="pt-5">
+                        <h2 className="text-2xl text-pri-500 font-medium py-5">JBL bar 2.1 deep Bass</h2>
+                        <h2 className="text-2xl">$11.70</h2>
+                        <div class="flex py-5 gap-2 text-gray-400">
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                        </div>
+                        <div className="">
+                            
+                        </div>
+                        
+                    </div>
+
+                </div>
+                <div class="w-1/2 flex flex-col gap-5">
+                    <div class="p-10 border border-[#B6B6B6] rounded-2xl flex-1">
+
+                    </div>
+                    <div class="p-10 border border-[#B6B6B6] rounded-2xl flex-1">
+
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
           </div>
         </>
     );
