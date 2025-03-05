9c0a7b69c8f81f98bce51767015c1c9866f6cd44
Al Mamun Khan <almamunkhan0101@gmail.com>
2025-03-06 01:39:43 +0600

    ain

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,9 +9,9 @@ function Header(){
             <div className="header">
                 <div className="container">
 
-                <div className="flex justify-between py-5">
+                <div className="flex justify-between py-2 lg:py-5 lg:text:normal text-xs">
                 <div className="text">
-                    <h3>
+                    <h3 className=" w-20">
                     Need help? Call us: (+98) 0234 456 789
                     </h3>
                 </div>
@@ -33,7 +33,7 @@ function Header(){
                 <div class="flex  gap-20">
                    <img src="./Images/logo.png" alt="logo" />
 
-                    <div class=" flex ">
+                    <div class=" flex justify-between ">
                         <input type="search" className="border-1 bg-white rounded-l-lg"/>
                         <button class="bg-sec-500 rounded-r-lg px-5 ">Searh</button>
                     </div>
@@ -60,12 +60,12 @@ function Header(){
 
          <div class="bg-[#F4F4F4]">
             <div class="">
-                <nav class=" container flex items-center justify-between">
+                <nav class=" container flex items-center justify-around text-sm text-center">
                     <div class="">
-                        <button class="border-1 border-transparent p-4 bg-sec-500 text-white ">Browse
+                        <button class="border-1 border-transparent p-2 bg-sec-500 text-white text-sm">Browse
                             Catagories</button>
                     </div>
-                    <ul class="flex items-center gap-15">
+                    <ul class="flex items-center gap-5 lg:gap-15">
                         <li><a href="">Home</a></li>
                         <li onClick={changePage}><a href="Catalog.html">Catalog</a></li>
                         <li><a href="">Blog</a></li>
@@ -82,15 +82,15 @@ function Header(){
         <div class="container flex justify-between items-center py-10">
          
             <div class="">
-                <h1 class="text-pri-500 text-7xl font-bold w-20 py-5">
+                <h1 class="text-pri-500 text-4xl lg:text-7xl font-bold w-20 py-5">
                     Canon Camera
                 </h1>
-                <div className="flex gap-5">
+                <div className="flex gap-1 lg:gap-5">
                     <button
-                        class="border-1 border-sec-500 text-white bg-sec-500 font-medium rounded-2xl p-3 hover:border-pri-500 hover:bg-transparent hover:text-pri-500">Shop
+                        class="border-1 border-sec-500 text-white bg-sec-500 font-medium rounded-2xl p-2 lg:p-3 hover:border-pri-500 hover:bg-transparent hover:text-pri-500">Shop
                         Now</button>
                     <button
-                        class="border-1 border-sec-500 text-white bg-sec-500 font-medium rounded-2xl p-3 hover:border-pri-500 hover:bg-transparent hover:text-pri-500">Read
+                        class="border-1 border-sec-500 text-white bg-sec-500 font-medium rounded-2xl p-2 lg:p-3 hover:border-pri-500 hover:bg-transparent hover:text-pri-500">Read
                         More</button>
 
                 </div>
@@ -105,8 +105,8 @@ function Header(){
             <div class="py-10 relative">
                 <img src="./Images/banner.png" alt="Camera_banner" />
                 <div
-                    class="bg-sec-500 w-35 h-35 rounded-full absolute bottom-15 -right-10 flex justify-center items-center">
-                    <h2 class="text-white text-2xl">
+                    class="bg-sec-500 w-20 h-20 lg:w-35 lg:h-35 rounded-full absolute bottom-15 right lg:-right-10 flex justify-center items-center">
+                    <h2 class="text-white  lg:text-2xl">
                         Only $89
                     </h2>
                 </div>
